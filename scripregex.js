97ec39ee4f3680dd414335eeedff30232b7b3f4a
Josh Curtiss <git@curtiss.me>
2017-01-04 02:35:16 -0600

    Initial commit of a scripture regex file.
    It is incomplete. Regex to find a Bible book for scripture matching. Does not do scripture ch:verse matching.

---
+++ scripregex.js
@@ -0,0 +1,70 @@
+[
+    {"regex":/Ge(n|nesis)?\.?\s/gi,                 "speak":"Genesis",              "name":"Genesis",       "num":1,    "fn":"ge"},
+    {"regex":/Ex(odus)?\.?\s/gi,                    "speak":"Exodus",               "name":"Exodus",        "num":2,    "fn":"ex"},
+    {"regex":/Le(v|viticus)?\.?\s/gi,               "speak":"Leviticus",            "name":"Leviticus",     "num":3,    "fn":"le"},
+    {"regex":/Nu(m|mber|mbers)?\.?\s/gi,            "speak":"Numbers",              "name":"Numbers",       "num":4,    "fn":"nu"},
+    {"regex":/De(ut|uter|uteronomy)?\.?\s/gi,       "speak":"Deuteronomy",          "name":"Deuteronomy",   "num":5,    "fn":"de"},
+    {"regex":/Jos(h|hua)?\.?\s/gi,                  "speak":"Joshua",               "name":"Joshua",        "num":6,    "fn":"jos"},
+    {"regex":/(Jg|Judg|Judge|Judges){1}\.?\s/gi,    "speak":"Judges",               "name":"Judges",        "num":7,    "fn":"jg"},
+    {"regex":/Ru(th)?\.?\s/gi,                      "speak":"Ruth",                 "name":"Ruth",          "num":8,    "fn":"ru"},
+    {"regex":/1\s?Sa(m|muel)?\.?\s/gi,              "speak":"First Samuel",         "name":"1 Samuel",      "num":9,    "fn":"1sa"},
+    {"regex":/2\s?Sa(m|muel)?\.?\s/gi,              "speak":"Second Samuel",        "name":"2 Samuel",      "num":10,   "fn":"2sa"},
+    {"regex":/1\s?Ki(ng|ngs)?\.?\s/gi,              "speak":"First Kings",          "name":"1 Kings",       "num":11,   "fn":"1ki"},
+    {"regex":/2\s?Ki(ng|ngs)?\.?\s/gi,              "speak":"Second Kings",         "name":"2 Kings",       "num":12,   "fn":"2ki"},
+    {"regex":/1\s?Ch(r|ron|ronicles)?\.?\s/gi,      "speak":"First Chronicles",     "name":"1 Chronicles",  "num":13,   "fn":"1ch"},
+    {"regex":/2\s?Ch(r|ron|ronicles)?\.?\s/gi,      "speak":"Second Chronicles",    "name":"2 Chronicles",  "num":14,   "fn":"2ch"},
+    {"regex":/Ezr(a|\.)?\s/gi,                      "speak":"Ezra",                 "name":"Ezra",          "num":15,   "fn":"ezr"},
+    {"regex":/Ne(h|hemiah)?\.?\s/gi,                "speak":"Neea my uh",           "name":"Nehemiah",      "num":16,   "fn":"ne"},
+    {"regex":/Es(t|th|ther)?\.?\s/gi,               "speak":"Esther",               "name":"Esther",        "num":17,   "fn":"es"},
+    {"regex":/Job\s/gi,                             "speak":"Joab",                 "name":"Job",           "num":18,   "fn":"job"},
+    {"regex":/Ps(alm|alms)?\.?\s/gi,                "speak":"Psalms",               "name":"Psalms",        "num":19,   "fn":"ps"},
+    {"regex":/Pr(o|ov|overb|overbs)?\.?\s/gi,       "speak":"Proverbs",             "name":"Proverbs",      "num":20,   "fn":"pr"},
+    {"regex":/Ec(c|cl|cles|clesiastes)?\.?\s/gi,    "speak":"Ecclesiastes",         "name":"Ecclesiastes",  "num":21,   "fn":"ec"},
+    {"regex":/(ca|can|canticles|song|song of sol|song of solomon)\.?\s/gi,
+                                                    "speak":"Song of Solomon", "name":"Song of Solomon", "num":22, "fn":"ca"},
+    {"regex":/Isa(iah|\.)?\s/gi,                    "speak":"Isaiah",               "name":"Isaiah",        "num":23,   "fn":"isa"},
+    {"regex":/Jer(emiah|\.)?\s/gi,                  "speak":"Jeremiah",             "name":"Jeremiah",      "num":24,   "fn":"jer"},
+    {"regex":/La(m|ment|mentation|mentations)?\.?\s/gi,
+                                                    "speak":"Lamentations",         "name":"Lamentations",  "num":25,   "fn":"la"},
+    {"regex":/Eze(k|kiel)?\.?\s/gi,                 "speak":"Ezekiel",              "name":"Ezekiel",       "num":26,   "fn":"eze"},
+    {"regex":/Da(n|niel)?\.?\s/gi,                  "speak":"Daniel",               "name":"Daniel",        "num":27,   "fn":"da"},
+    // TODO: Hosea
+    // TODO: Joel
+    /* Amos */
+    // TODO: Obadiah
+    // TODO: Jonah
+    //{"regex":/Mic\.\s/gi,"speak":"Micah"},
+    // TODO: Nahum
+    // TODO: Habakkuk
+    // TODO: Zeph
+    //{"regex":/Hag\.\s/gi,"speak":"Hag eye"},
+    //{"regex":/Zech\.\s/gi,"speak":"Zechariah"},
+    //{"regex":/Mal\.\s/gi,"speak":"Malachi"},
+    //{"regex":/Matt\.\s/gi,"speak":"Matthew"},
+    /* Mark */
+    /* Luke */
+    /* John */
+    /* Acts */
+    //{"regex":/Rom\.\s/gi,"speak":"Romans"},
+    //{"regex":/1\sCor\.\s/gi,"speak":"First Corinthians"},
+    //{"regex":/2\sCor\.\s/gi,"speak":"Second Corinthians"},
+    //{"regex":/Gal\.\s/gi,"speak":"Galatians"},
+    //{"regex":/Eph\.\s/gi,"speak":"Ephesians"},
+    //{"regex":/Phil\.\s/gi,"speak":"Phillipians"},
+    //{"regex":/Col\.\s/gi,"speak":"Colossians"},
+    //{"regex":/1\sThess\.\s/gi,"speak":"First Thessalonians"},
+    //{"regex":/2\sThess\.\s/gi,"speak":"Second Thessalonians"},
+    //{"regex":/1\sTim\.\s/gi,"speak":"First Timothy"},
+    //{"regex":/2\sTim\.\s/gi,"speak":"Second Timothy"},
+    // TODO: Titus
+    // TODO: Philemon
+    //{"regex":/Heb\.\s/gi,"speak":"Hebrews"},
+    //{"regex":/Jas\.\s/gi,"speak":"James"},
+    //{"regex":/1\sPet\.\s/gi,"speak":"First Peter"},
+    //{"regex":/2\sPet\.\s/gi,"speak":"Second Peter"},
+    //{"regex":/1\sJohn\s/gi,"speak":"First John"},
+    //{"regex":/2\sJohn\s/gi,"speak":"Second John"},
+    //{"regex":/3\sJohn\s/gi,"speak":"Third John"},
+    // TODO: Jude
+    {"regex":/Re(v|velation)?\.?\s/gi,          "speak":"Revelation",               "name":"Revelation",    "num":66,   "fn":"re"}
+]
