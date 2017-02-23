0355ed51f62d726ec1d4870e405011b6b256c59e
Josh Curtiss <git@curtiss.me>
2017-02-23 00:31:25 -0600

    Strip video features from ScriptureUtil.
    The video features can go back into the khplaylist repo and this library stays cleaner, not handling any video or WebVTT stuff.

--- ScriptureUtil.js
+++ ScriptureUtil.js
@@ -1,33 +1,10 @@
 let Scripture=require("./Scripture.js");
-let ScriptureVideo=require("./ScriptureVideo.js");
 let BibleBook=require("./BibleBook.js");
-let fs=require("fs-extra");
 
 class ScriptureUtil {
 
-    constructor(videopath) {
-        // Video path is the path under which all video files are housed. Subdirectories ok.
-        this.videopath=videopath;
+    constructor() {
         return this;
-    }
-
-    get videopath(){return this._videopath}
-    set videopath(path) {
-        this._videopath=path||"";
-        // Keep a list of all the videos
-        var pathwalk=[];
-        try {
-            pathwalk=fs.walkSync(this.videopath);
-        } catch(err) {}
-        var videos=[], webvtts=[], f, ext;
-        for( var path of pathwalk ) {
-            f=path.split("/").pop();
-            ext=f.split(".").pop().toLowerCase();
-            if( ScriptureUtil.VIDEOEXT.indexOf(ext)>=0 ) videos.push(path);
-            else if( ScriptureUtil.WEBVTTEXT.indexOf(ext)>=0 ) webvtts.push(path);
-        }
-        this.videos=videos.reverse(); // Reverse to get higher def versions as first choice.
-        this.webvtts=webvtts;
     }
 
     /*  
@@ -70,42 +47,10 @@ class ScriptureUtil {
         return b;
     }
 
-    /*
-     *  createVideo: Receives a scripture and returns a ScriptureVideo object
-     *  that knows how to exactly play that scripture from the video.
-     * 
-     */
-    createVideo(scripture) {
-        // RegEx: /something_BOOKNUM_BOOKSYMBOL_something_CHAPTER_r999p.ext
-        if( scripture.valid() ) {
-            var booknum=(scripture.book.num<10?"0":"")+scripture.book.num.toString();
-            var chapter=(scripture.chapter<10?"0":"")+scripture.chapter.toString();
-            var baseFileRegex=
-                "\/\\w+_"+
-                booknum+"_"+
-                scripture.book.symbol+"_"+
-                "\\w+_"+
-                chapter+"_"+
-                "r\\d{3}p\\.\\w{3}";
-            var videoRegex=new RegExp(baseFileRegex+"$","i");
-            var webvttRegex=new RegExp(baseFileRegex+"\\.\\w{3,6}$","i");
-            var videoFile=this.videos.find((value)=>{return videoRegex.test(value)});
-            var webvttFile=this.webvtts.find((value)=>{return webvttRegex.test(value)});
-            // TODO: Handle if file doesn't exist, create webvtt on the fly.
-            var webvtt="";
-            try {
-                webvtt=fs.readFileSync(webvttFile,"UTF-8");
-            } catch (error) {}
-            return new ScriptureVideo(scripture,videoFile,webvtt);
-        }
-    }
-
 }
 
 ScriptureUtil.SCRIPTURE_REGEX = /\b(\w+)\s*(\d[\d\s\-\.,;:]*)/igm;
 ScriptureUtil.CHAPTERVERSE_REGEX = /(\d+)\s*[:\.]([\d\s\-,]+)/g;
-ScriptureUtil.VIDEOEXT = ["mp4","m4v","mov"];
-ScriptureUtil.WEBVTTEXT = ["webvtt","vtt"];
 ScriptureUtil.BIBLEBOOKS = [
     new BibleBook(  1,  "Genesis",      "ge",   "Genesis",              /Ge(?:n|nesis)?\.?/i   ),
     new BibleBook(  2,  "Exodus",       "ex",   "Exodus",               /Ex(?:odus)?\.?/i ),
