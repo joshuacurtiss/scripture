c1192b13d5347ce2e55abf7433ed84eb9a12eccc
Josh Curtiss <git@curtiss.me>
2019-03-16 01:37:11 -0500

    Add some more scripture tests.

--- test/scriptureutil.js
+++ test/scriptureutil.js
@@ -43,7 +43,8 @@ describe("ScriptureUtil", function() {
             {text:"Here is Gen 3:15 as the first prophecy.", cnt:1},
             {text:"The first prophecy is Gen 3:15.", cnt:1},
             {text:"Try Gen 3:18-20, 23; 8:4-6 for more info.", cnt:2},
-            {text:"Ps 119:105; John 1:1; John 3:16; 5:12-15, 18", cnt:4}
+            {text:"Ps 119:105; John 1:1; John 3:16; 5:12-15, 18", cnt:4},
+            {text:"Now try Jude 2; Philemon 3; Gen 3:10 and Gen 4:10.", cnt:4}
         ];
         tests.forEach(function(test) {
             it(`should find ${test.cnt} match${test.cnt!=1?"es":""} in "${test.text}"`, function() {
